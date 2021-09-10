a6524877948a8908260eaad0750dc0fa4d813d51
glocore <mailtoashwinnayar@gmail.com>
2021-09-10 23:53:45 +0530

    update to use unsplash API to populate first paint

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,68 +4,24 @@ import { SearchPanel } from "../components/SearchPanel/SearchPanel";
 import { Thumbnail } from "../components/Thumbnail";
 import { ThumbnailGrid } from "../components/ThumbnailGrid";
 
-const Home: NextPage = () => {
+const Home: NextPage<{ initialCollection?: CollectionItem[] }> = ({
+  initialCollection,
+}) => {
   return (
     <Box mr={8} ml={8} minH="100vh">
       <SearchPanel onChange={console.log} />
       <ThumbnailGrid>
         <>
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
-          <Thumbnail
-            imageUrl="https://images.unsplash.com/photo-1518331539918-7a2dbf839306?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxODk0NjZ8MHwxfGNvbGxlY3Rpb258MXwyNDIzNTY5fHx8fHwyfHwxNjMxMTcyMzgz&ixlib=rb-1.2.1&q=80&w=1080"
-            blurhash="L77m$p9#ENxWE3xWs:WX0%-TxYNH"
-            height={300}
-            width="100%"
-            altDescription="LEGO Star Wars toy"
-          />
+          {initialCollection?.map((collectionItem) => (
+            <Thumbnail
+              key={collectionItem.id}
+              imageUrl={collectionItem.urls.regular}
+              blurhash={collectionItem.blur_hash}
+              altDescription={collectionItem.alt_description}
+              height={300}
+              width="100%"
+            />
+          ))}
         </>
       </ThumbnailGrid>
     </Box>
@@ -73,3 +29,37 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+type CollectionItem = {
+  id: string;
+  blur_hash: string;
+  alt_description: string;
+  urls: {
+    regular: string;
+  };
+};
+
+export async function getStaticProps() {
+  const headers = new Headers();
+  headers.append(
+    "Authorization",
+    "Client-ID FZpIl7feLseFHwV4DScQqiaVULO54C7GRBiqlmDrxdI"
+  );
+
+  const requestOptions = {
+    method: "GET",
+    headers: headers,
+  };
+
+  const responseJson = await fetch(
+    "https://api.unsplash.com/collections/2423569/photos",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .catch((error) => console.log("error", error));
+  return {
+    props: {
+      initialCollection: responseJson,
+    },
+  };
+}
